d94898fade5dd47159ff3822217c0790806f616f
Adriano Novais Queiroz <adriano.nq@gmail.com>
2021-06-19 16:53:05 -0300

    Feature TODO List completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // Cria uma nova task com um id random, não permite criar caso o título seja vazio.
+    if(!newTaskTitle) return; 
+     
+    const newTask: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+    
+    setNewTaskTitle('');
+    setTasks([...tasks, newTask]);  
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    // Altera entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const updatedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+    
+    setTasks(updatedTasks);       
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
